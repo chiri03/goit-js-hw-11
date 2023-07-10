e8ea14d79f4a630c4962becfc3787159087b605a
chiri03 <lena.kiriac.97@gmail.com>
2023-07-10 21:28:30 +0300

    add js async/await function

--- src/index.js
+++ src/index.js
@@ -11,3 +11,64 @@ loadMoreBtn.style.display = 'none';
 loadMoreBtn.addEventListener('click', loadMoreImages);
 form.addEventListener('submit', searchImage);
 
+async function searchImage(e) {
+  try {
+    e.preventDefault();
+    resetMarkup();
+    const keyword = e.currentTarget.elements.searchQuery.value.trim();
+    AxiosApi.query = keyword;
+    AxiosApi.page = 1;
+    AxiosApi.hits = 0;
+
+    e.currentTarget.reset();
+
+    const data = await AxiosApi.fetchImage();
+
+    renderGallery(data);
+    loadMoreBtn.style.display = block;
+
+    if (!data.hits.length) {
+      loadMoreBtn.style.display = none;
+      return Notiflix.Notify.failure(
+        "Sorry, there are no images matching your search query. Please try again."
+      );
+    }
+    if (data.totalHits <= 41) {
+      loadMoreBtn.style.display = "none";
+      return Notiflix.Notify.success(
+        `Hooray! We found ${data.totalHits} images.`
+      );
+    } else {
+      return Notiflix.Notify.success(
+        `Hooray! We found ${data.totalHits} images.`
+      );
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function resetMarkup(){
+  gallery.innerHTML = "";
+}
+
+async function loadMoreImages(e) {
+  try {
+    AxiosApi.page += 1;
+    const data = await AxiosApi.fetchImage();
+    if (data.hits.length === 0) {
+      loadMoreBtn.style.display = 'none';
+      return Notiflix.Notify.info(
+        `We're sorry, but you've reached the end of the results.`
+      );
+    }
+    renderGallery(data);
+  } catch (error) {
+    loadMoreBtn.style.display = 'none';
+    Notiflix.Notify.info(
+      `We're sorry, but you've reached the end of the results.`
+    );
+    console.log(error);
+  }
+}
